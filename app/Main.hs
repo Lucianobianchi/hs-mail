d7af7260b7c12fb51d6d73d7edbb327396c92ffb
Luciano Bianchi <lusho@fb.com>
2019-11-28 17:49:42 -0300

    one server in each thread

--- app/Main.hs
+++ app/Main.hs
@@ -1,21 +1,24 @@
 module Main (main) where
 
 import Control.Concurrent (forkFinally, forkIO)
+import Control.Concurrent.MVar
 --import Control.Concurrent.Thread (wait)
 import qualified Control.Exception as E
 import Network.Socket
 import Control.Monad (unless, forever, void)
 import SMTP.Server(smtpServer)
 import POP.Server(popServer)
--- import POP.Server(popServer)
 
--- main :: IO ()
--- main = do 
-main = runTCPServer Nothing "25" smtpServer
---   return ()
--- TODO: manejar mensajes vacíos
+smtpRun = runTCPServer Nothing "25" smtpServer
+popRun = runTCPServer Nothing "110" popServer
 
--- main = runTCPServer Nothing "110" popServer -- TODO: ver puerto
+main = do
+  smtpMvar <- newEmptyMVar
+  popMvar <- newEmptyMVar
+  forkFinally smtpRun (\_ -> putMVar smtpMvar ())
+  forkFinally popRun (\_ -> putMVar popMvar ())
+  readMVar smtpMvar
+  readMVar popMvar
 
 -- TODO: entender mejor que hace esto
 -- from the "network-run" package.
